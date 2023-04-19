d0e5045af9c77f9c65cbca87680ad585238e3e4a
kdivyans <kdivyans@cisco.com>
2023-04-20 02:52:39 +0530

    pages and cart functionality added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,17 @@
 import { Layout } from "@/components";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { StateContext } from "@/context/StateContext";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      {/* this below <Component ...> is the current component that we are on, 
+    <StateContext>
+      <Layout>
+        {/* this below <Component ...> is the current component that we are on, 
       e.g. if we are on any product page then component is the product,
       if we are on index then index */}
-      <Component {...pageProps} />
-    </Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
   );
 }
